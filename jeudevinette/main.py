dbdabc914a255c6f73283038f0a8ed71039700f3
pellandma <143745703+pellandma@users.noreply.github.com>
2023-10-13 11:35:15 -0400

    Update main.py

--- jeudevinette/main.py
+++ jeudevinette/main.py
@@ -1,31 +1,36 @@
-#TP2 jeu de devinettes
+# TP2 jeu de devinettes
 
 import random
 
+x = random.randint(0, 100)
+nb_essai = 0
+
 def JeuDeDevinette():
-    
-    nb_essai = 0
-    x = random.randint(0, 100)
+
+
     print("j'ai choisi un nombre entre 0 et 100." "\nA vous de deviner: ")
 
     essai = int(input())
-    nb_essai + 1 
+
 
     if essai < x:
         print("Mauvaise reponse, c'est plus haut")
+        nb_essai = + 1
         JeuDeDevinette()
 
     if essai > x:
         print('mauvaise reponse, elle est plus basse ')
+        nb_essai = + 1
         JeuDeDevinette()
-        
+
     if essai == x:
-        print("Bravo! Bonne reponse, vous avez reussi en: ", (nb_essai)) 
-        
+        print("Bravo! Bonne reponse, vous avez reussi en: ", nb_essai)
 
         quit = input("voulez vous quitter: y/n\n")
         if quit == "y":
             print("merci et au revoir")
         if quit == "n":
             JeuDeDevinette()
+
+
 JeuDeDevinette()
